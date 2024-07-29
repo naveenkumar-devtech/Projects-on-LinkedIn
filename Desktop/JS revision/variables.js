8dcdab318e08a5cfd14dafb8c386cab55fc6e819
Naveen Kumar <naveentyut@gmail.com>
2024-07-29 21:23:54 +0530

    Variables in JS

---
+++ Desktop/JS revision/variables.js
@@ -0,0 +1,58 @@
+
+// scope of the variable
+// reassignment
+// redeclaration
+// mutability ---------> changes allowed
+// immutability -------> changes not allowed
+// closures
+// hosting
+
+
+
+// function variable(){
+//     var a=5
+//     console.log(a)
+// }
+
+// variable()
+// console.log(a)
+
+
+// a=20
+// function variable(){
+//    { var a=500
+//     console.log(a)}
+// console.log(a)
+
+// }
+
+// variable()
+// console.log(a)
+
+// console.log(a)
+// var a=5
+// console.log(a)
+
+
+a=5 
+a=10             //var is default because of mutability,reassignment,redeclare 
+console.log(a)
+
+
+
+c=20
+function variable(){
+    console.log(c)
+}
+variable()
+console.log(c)
+
+
+
+
+
+
+
+
+
+
