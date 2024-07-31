c43169a378a8f0a52bb157c66f892162fdf8377e
Naveen Kumar <naveentyut@gmail.com>
2024-07-31 20:09:50 +0530

    Updated

---
+++ Desktop/JS revision/function_flow.js
@@ -0,0 +1,24 @@
+function sum(){
+    console.log('6')
+    console.log('7')
+
+
+}
+
+function main(){
+  setTimeout(() =>{
+    console.log('1')
+    console.log('2')
+    console.log('3')
+    console.log('4')
+    sum()
+    console.log('5')
+  },3000)
+
+
+}
+
+
+main();
+console.log('22')
+console.log('23')
